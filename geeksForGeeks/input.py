b01838412d0cb12725a8bde95ead587467596cb9
goodTimes <andresrey007@gmail.com>
2025-12-20 11:16:02 -0800

    One more Exercise

---
+++ geeksForGeeks/input.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+
+# Take integer input and add 10 to the integer input and print
+
+# Take floating-point input and multiply the float input by 10 and print
+
+########### Write your code above ###############
+s = "Hello"
+n = 20
+f = 5.5
+print(str(s))
+print(int(n)+10)
+print(float(f)*10)
